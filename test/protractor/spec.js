21ba624d372a895de215fbcee012eb9a43c99cdf
jonathanro <jonathan_dfi@hotmail.com>
2015-06-30 13:09:54 -0300

    protractor!?

--- test/protractor/spec.js
+++ test/protractor/spec.js
@@ -1,17 +1,16 @@
 describe('Protractor', function() {
-    it('should have a title', function() {
-        browser.get('https://morning-castle-6615.herokuapp.com/precoLamborghini/');
 
-        expect(browser.getTitle()).toEqual('Parcelas de Carros');
-    });
 
-    it('should add one and two', function() {
-        browser.get('https://morning-castle-6615.herokuapp.com/precoLamborghini');
-        element(by.model('dado')).sendKeys(50);
+    describe('Protractor Demo App', function() {
+        it('should add one and two', function() {
+            browser.get('http://juliemr.github.io/protractor-demo/');
+            element(by.model('first')).sendKeys(1);
+            element(by.model('second')).sendKeys(2);
 
-        element(by.id('botao')).click();
+            element(by.id('gobutton')).click();
 
-        expect(element(by.binding('resultado')).getText()).
-            toEqual('59800.00');
+            expect(element(by.binding('latest')).getText()).
+                toEqual('3'); // This is wrong!
+        });
     });
 });
